3aaaa0ec8d56962d95ac103918f2c8e01581992c
Sander Borret <sander.borret@student.kdg.be>
2018-12-06 00:46:38 +0100

    change some routing details

--- site/config/database.php
+++ site/config/database.php
@@ -35,7 +35,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => env('DB_DATABASE', database_path('musicalloo.db')),
+            'database' => database_path('musicalloo.db'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
--- site/routes/web.php
+++ site/routes/web.php
@@ -11,12 +11,10 @@
 |
 */
 
-
-
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@home')->name('home');
+Route::redirect('/home', '/');
 
 Route::get('/map', 'HomeController@map')->name('map');
 Route::get('/faq', 'HomeController@faq')->name('faq');
