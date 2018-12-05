89d23db53edb4eeb27c2f9c896456259b8f0d496
Sander Borret <sander.borret@student.kdg.be>
2018-12-05 23:17:51 +0100

    add default admin seeder

--- site/config/app.php
+++ site/config/app.php
@@ -2,6 +2,10 @@
 
 return [
 
+    'admin_name' => env('ADMIN_NAME', null),
+    'admin_email' => env('ADMIN_EMAIL', null),
+    'admin_password' => env('ADMIN_PASSWORD', null),
+
     /*
     |--------------------------------------------------------------------------
     | Application Name
