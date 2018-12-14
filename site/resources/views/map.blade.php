277d10e8211d8e8dbebf28afd2a2f4861dcc9726
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-12-14 11:23:18 +0100

    added a map

--- site/resources/views/map.blade.php
+++ site/resources/views/map.blade.php
@@ -3,5 +3,33 @@
 @section('title', 'Map')
 
 @section('content')
-    <h1>Map</h1>
+	<div class="mapcontainer">
+	    <h1>Map</h1>
+
+	    <div id="mapid"></div>
+    </div>
+    <?php echo config('app.mapbox_token') ?>
+@endsection
+
+
+
+
+@section('custom-scripts')
+	<script>
+		var mymap = L.map('mapid').setView([51.189260, 4.377368], 14);
+
+		L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+		    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		    maxZoom: 18,
+		    id: 'mapbox.streets',
+		    accessToken: '{{ env('MAPBOX_TOKEN') }}'
+		}).addTo(mymap);
+
+
+		var KdGHoboken = L.marker([51.173339, 4.371204]).addTo(mymap);
+
+		KdGHoboken.bindPopup("<b>KdG Hoboken</b><br>Een school");
+
+
+	</script>
 @endsection
