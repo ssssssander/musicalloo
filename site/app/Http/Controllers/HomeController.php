9be0c75bd5bb3e75937e62a1f5de53c73c6ca138
Niels van Nimmen <niels.van.nimmen@gmail.com>
2018-12-14 13:18:56 +0100

    added map function to homecontroller

--- site/app/Http/Controllers/HomeController.php
+++ site/app/Http/Controllers/HomeController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Marker;
 
 class HomeController extends Controller
 {
     public function home()
     {
         return view('home');
+    }
+
+
+    public function map()
+    {
+        $markers = Marker::all();
+
+        return view('map', compact('markers'));
     }
 
 
