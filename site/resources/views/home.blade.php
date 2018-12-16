b3029f6d2a2f8a2dc3c6d25e8dde33ec0d878e81
LazloDB <lazlodebock@hotmail.com>
2018-12-16 13:27:28 +0100

    new design

--- site/resources/views/home.blade.php
+++ site/resources/views/home.blade.php
@@ -1,7 +1,17 @@
-@extends('layouts.main')
+@extends('layouts.app')
 
 @section('title', 'Home')
 
-@section('content')
-    <h1>Welcome to our humble home page</h1>
+@section('main')
+    <main>
+        <h1>Musicalloo</h1>
+        <div>I sit and shit on my toilet when I hear a funny sound. Another person comes in and sits on a toilet. The sound is getting richer. What is this feeling of joy? It must be Musicalloo. (Placeholder text)</div>
+    </main>
+
+    <div class="toilet-wrapper">
+        <img class="music-note1" src="svg/music_note.svg" alt="note">
+        <img class="music-note2" src="svg/music_note.svg" alt="note">
+        <img class="music-note3" src="svg/music_note.svg" alt="note">
+        <img class="toilet-image" src="svg/toilet.svg" alt="toilet">
+    </div>
 @endsection
--- site/resources/views/layouts/app.blade.php
+++ site/resources/views/layouts/app.blade.php
@@ -26,6 +26,7 @@
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.4/dist/leaflet.css"
    integrity="sha512-puBpdR0798OZvTTbP4A8Ix/l+A4dHDD0DGqYW6RQ+9jxkRFclaxxQb/SJAWZfWAkuyeQUytO7+7N4QKrDh+drA=="
    crossorigin=""/>
+
     <!-- Script -->
     <script src="https://unpkg.com/leaflet@1.3.4/dist/leaflet.js"
    integrity="sha512-nMMmRyTVoLYqjP9hrbed9S+FzjZHW5gY1TWCHA5ckwXZBadntCNs8kEqAWdrb9O7rxbCaA4lKTIWjDXZxflOcA=="
@@ -34,70 +35,23 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
-                <a class="navbar-brand" href="{{ route('home') }}">
-                    {{ config('app.name', 'Musicalloo') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('map') }}">Map</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('faq') }}">FAQ</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('contact') }}">Contact</a>
-                        </li>
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                        @else
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('music.index') }}">Music</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('marker.index') }}">Markers</a>
-                            </li>
-                            <li class="nav-item">
-                                @if (Route::has('register'))
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                @endif
-                            </li>
-
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
+        <nav>
+            <img class="nav-image" src="svg/navigation2.svg" alt="navigation">
+            <a class="nav_item" href="{{ route('home') }}">
+                <img src="svg/home_icon.svg" alt="homeIcon">
+            </a>
+            <a class="nav_item">
+                <img src="svg/music_icon.svg" alt="musicIcon">
+            </a>
+            <a class="nav_item" href="{{ route('map') }}">
+                <img src="svg/marker_icon.svg" alt="markerIcon">
+            </a>
+            <a class="nav_item" href="{{ route('faq') }}">
+                <img src="svg/faq_icon.svg" alt="faqIcon">
+            </a>
+            <a class="nav_item" href="{{ route('contact') }}">
+                <img src="svg/mail_icon.svg" alt="mailIcon">
+            </a>
         </nav>
         @yield('main')
     </div>
