c3011b23b5c22efe3d95fea7455b3b3252689709
Katriena Banasik <katriena.0107@hotmail.com>
2018-12-22 22:11:24 +0100

    redesign

--- site/resources/views/home.blade.php
+++ site/resources/views/home.blade.php
@@ -5,14 +5,14 @@
 @section('main')
     <main>
         <h1>Musicalloo</h1>
-        <span class="divider"></span>
         <div class="content">I sit and shit on my toilet when I hear a funny sound. Another person comes in and sits on a toilet. The sound is getting richer. What is this feeling of joy? It must be Musicalloo. (Placeholder text)</div>
-    </main>
 
-    <div class="toilet-wrapper">
-        <img class="music-note1" src="svg/music_note.svg" alt="note">
-        <img class="music-note2" src="svg/music_note2.svg" alt="note">
-        <img class="music-note3" src="svg/music_note2.svg" alt="note">
-        <img class="toilet-image" src="svg/toilet.svg" alt="toilet">
-    </div>
+        <div class="toilet-wrapper">
+            <img class="music-note1" src="svg/music_note.svg" alt="note">
+            <img class="music-note2" src="svg/music_note2.svg" alt="note">
+            <img class="music-note3" src="svg/music_note2.svg" alt="note">
+            <img class="toilet-image" src="svg/toilet.svg" alt="toilet">
+        </div>
+
+    </main>
 @endsection
