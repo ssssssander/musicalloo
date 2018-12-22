466cbc5f4c39fec0f52c787370befa78a96113c7
Katriena Banasik <katriena.0107@hotmail.com>
2018-12-22 22:32:40 +0100

    written homepage text and cleaned up code some more

--- site/resources/views/home.blade.php
+++ site/resources/views/home.blade.php
@@ -5,7 +5,18 @@
 @section('main')
     <main>
         <h1>Musicalloo</h1>
-        <div class="content">I sit and shit on my toilet when I hear a funny sound. Another person comes in and sits on a toilet. The sound is getting richer. What is this feeling of joy? It must be Musicalloo. (Placeholder text)</div>
+        <div class="content">
+            <p>
+                Did you know you spend about 92 entire days sitting on the toilet? 92 entire days - it's no wonder you try looking for something fun to do, like reading a book or checking your ex's embarrassing twitter updates.
+            </p>
+            <p class="large_p">
+                So why not make something cool out of the toilet experience?
+                <br><span class="">That's what we did.</span>
+            </p>
+            <p>
+                Musicalloo is where toilets meet music. It's simple: you sit down on the toilet, and the music begins to play. If someone else enters the stall next to you, the music gets richer. <a href="{{ url('/faq') }}">Want to know how it works?</a>
+            </p>
+        </div>
 
         <div class="toilet-wrapper">
             <img class="music-note1" src="svg/music_note.svg" alt="note">
