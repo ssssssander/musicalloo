60196c04e62de3b4b446739726d411cfb7940545
LazloDB <lazlodebock@hotmail.com>
2018-12-27 00:31:43 +0100

    added admin nav

--- site/resources/views/layouts/app.blade.php
+++ site/resources/views/layouts/app.blade.php
@@ -40,6 +40,42 @@
     @else
         @yield('main')
     @endif
+
+    <!-- Admin nav -->
+    @guest
+    @else
+        <ul class="admin-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('music.index') }}">Music</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('marker.index') }}">Markers</a>
+            </li>
+            <li class="nav-item">
+                @if (Route::has('register'))
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                @endif
+            </li>
+
+            <li class="nav-item dropdown">
+                <!-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }} <span class="caret"></span>
+                </a> -->
+
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        </ul>
+    @endguest
     </div>
     @yield('custom-scripts')
 
